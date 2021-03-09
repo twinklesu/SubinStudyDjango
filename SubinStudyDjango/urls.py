bb5fb581f26ccdce0775f3c35cdc96d09cc6b8d2
twinklesu <twinklesu914@gmail.com>
2021-03-09 23:59:07 +0900

    FVB로 랜딩페이지, 자기소개 페이지 만들기

--- SubinStudyDjango/urls.py
+++ SubinStudyDjango/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('single_pages.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
