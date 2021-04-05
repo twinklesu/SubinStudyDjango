8d0c36e063700116258ab8a55f2c97b7a0aabff7
twinklesu <twinklesu914@gmail.com>
2021-04-05 17:15:00 +0900

    댓글 폼으로 포스트 상세 페이지에서 댓글 작성하기

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns =[
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('<int:pk>/new_comment/', views.new_comment),
     # path('<int:pk>/', views.single_post_page),
     path('', views.PostList.as_view()),
     # path('', views.index),
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
 
 # CVB
 
@@ -26,6 +28,7 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['comment_form'] = CommentForm
         return context
 
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -138,6 +141,23 @@ def tag_page(request, slug):
             'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
+
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
