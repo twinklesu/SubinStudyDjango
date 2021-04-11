062a52271e44228c5e5a5730e6c93d96b738525a
twinklesu <twinklesu914@gmail.com>
2021-04-11 21:24:11 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -83,3 +83,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/109/fb63a6424b6a0a51/svg/{self.author.email}'
